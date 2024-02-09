e84c5ba5eb09be10ceb5ab13b3c3d95aa0bfd003
Filipe Vilas Bôas <fvb.boas@gmail.com>
2024-02-09 18:05:02 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"https://c4.wallpaperflare.com/wallpaper/350/1006/490/cat-4k-1080p-high-quality-wallpaper-preview.jpg",
+        "title":"GATOS COMEM FEIJÃO",
+        "description": "Gato amarelo na grama verde te observa enquanto espera pelo pássaro amarelo, o que será que ele vai fazer quando a ave chegar?"
+    },
+    {
+        "id":2,
+        "photo":"https://img.freepik.com/fotos-gratis/captura-horizontal-de-dois-gatos-sentados-em-um-fundo-desfocado_181624-56173.jpg",
+        "title":"saiu outro filme",
+        "description":"tchau"
+    },
+    {
+        "id":3,
+        "photo":"https://img.freepik.com/fotos-gratis/close-de-um-gato-ruivo-abracando-e-lambendo-o-outro-isolado-em-uma-parede-branca_181624-32893.jpg",
+        "title":"saiu outro filme",
+        "description":"tchau"
+    },
+    {
+        "id":4,
+        "photo":"https://img.freepik.com/fotos-premium/imagem-de-alta-qualidade-de-um-belo-gato-manx_1008702-1569.jpg ",
+        "title":"saiu outro filme",
+        "description":"tchau"
+    }
+]
